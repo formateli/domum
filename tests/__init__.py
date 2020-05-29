3ae8c7f2aef240e39aa70a783ec492be070f597b
formateli <formateli@gmail.com>
2020-05-29 15:43:17 -0500

    Fixes

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,10 @@
+# This file is part of domum module.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 try:
-    from trytond.modules.domum.tests.domum_test import suite
+    from trytond.modules.domum.tests.domum_test import \
+            suite, create_group_units
 except ImportError:
-    from .domum_test import suite
+    from .domum_test import suite, create_group_units
 
-__all__ = ['suite']
+__all__ = ['suite', 'create_group_units']
--- tests/domum_test.py
+++ tests/domum_test.py
@@ -1,3 +1,4 @@
+# This file is part of domum module.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 import unittest
@@ -5,6 +6,8 @@ import trytond.tests.test_tryton
 from trytond.pool import Pool
 from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.modules.company.tests import create_company, set_company
+
+__all__ = ['create_group_units']
 
 
 class DomumTestCase(ModuleTestCase):
@@ -14,10 +17,7 @@ class DomumTestCase(ModuleTestCase):
     @with_transaction()
     def test_domum(self):
         pool = Pool()
-        Party = pool.get('party.party')
         Configuration = pool.get('domum.configuration')
-        Group = pool.get('domum.group')
-        Unit = pool.get('domum.unit')
         Extension = pool.get('domum.unit.extension')
         UnitOwner = pool.get('domun.unit-party.owner')
         UnitResident = pool.get('domun.unit-party.resident')
@@ -25,32 +25,49 @@ class DomumTestCase(ModuleTestCase):
 
         company = create_company()
         with set_company(company):
-            building, = Group.create([{
-                'company': company,
-                'name': 'Building',
-                }])
-            i = 1
-            while i < 11:
-                floor, = Group.create([{
-                    'company': company,
-                    'parent': building.id,
-                    'name': 'Floor ' + str(i),
-                    }])
+            building, units = create_group_units(company)
 
-                x = 1
-                while x < 5:
-                    party, = Party.create([{
-                        'name': 'Apartment ' + str(x) + ' ' + floor.name,
-                        'party_type': 'domum',
-                        }])
-                    Unit.create([{
-                        'group': floor.id,
-                        'type': 'apartment',
-                        'party': party.id,
-                        'state': 'unknown',
-                        }])
-                    x += 1
-                i += 1
+
+def create_group_units(company):
+    pool = Pool()
+    Party = pool.get('party.party')
+    Address = pool.get('party.address')
+    Group = pool.get('domum.group')
+    Unit = pool.get('domum.unit')
+
+    group, = Group.create([{
+        'company': company,
+        'name': 'Domum Group',
+        }])
+
+    units = []
+    i = 1
+    while i < 11:
+        floor, = Group.create([{
+            'company': company,
+            'parent': group.id,
+            'name': 'Floor ' + str(i),
+            }])
+
+        x = 1
+        while x < 5:
+            party = Party(
+                name='Apartment ' + str(x) + ' ' + floor.name,
+                party_type='domum',
+                addresses=[Address(name='Main Address')]
+                )
+            party.save()
+
+            units += Unit.create([{
+                    'group': floor.id,
+                    'type': 'apartment',
+                    'party': party.id,
+                    'state': 'unknown',
+                    }])
+            x += 1
+        i += 1
+
+    return group, units
 
 
 def suite():
